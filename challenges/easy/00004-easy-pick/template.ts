a68cdd29c084878164e4d0e6d013f3532ee69231
wliang <wliang@infervision.com>
2022-09-20 14:27:32 +0800

    feat: 完成 00004-easy-pick

--- challenges/easy/00004-easy-pick/template.ts
+++ challenges/easy/00004-easy-pick/template.ts
@@ -1,1 +1,22 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+/**
+ * 返回一个对象
+ * 遍历keys
+ * 如果key在todo里
+ * 则将值赋给新的对象
+ */
+
+// function myPick (todo, keys){
+//   const obj = {}
+
+//   keys.forEach(key => {
+//     if(key in todo){
+//       obj[key] = todo[key]
+//     }
+//   });
+
+//   return obj
+// }
