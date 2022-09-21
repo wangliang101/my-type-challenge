bd50ae6ca8a2d8abf395a672efc4c993ab76f16c
wliang <wliang@infervision.com>
2022-09-21 13:29:41 +0800

    feat: readonly and tuple to obj

--- challenges/easy/00011-easy-tuple-to-object/template.ts
+++ challenges/easy/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,19 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P;
+};
+
+// type r = TupleToObject<typeof tuple>;
+
+/**
+ * 1、返回一个对象
+ * 2、遍历这个数据，形成key:value结构
+ */
+// function tupleToObject(tuple){
+//   const obj = {}
+
+//   tuple.forEach(v => {
+//     obj[v] = v
+//   });
+
+//   return obj
+// }
