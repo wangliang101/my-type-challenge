5c8f9d25e12de049742c4e3b9ce8fed3037e8491
wliang <wliang@infervision.com>
2022-09-24 18:42:55 +0800

    feat: includes

--- challenges/easy/00898-easy-includes/template.ts
+++ challenges/easy/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+// type Includes<T extends readonly any[], U> = any
+
+type IsEqual<A, B> = (<X>() => X extends A ? 1 : 2) extends <X>() => X extends B ? 1 : 2 ? true : false;
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rust]
+  ? true extends IsEqual<First, U>
+    ? true
+    : Includes<Rust, U>
+  : false;
