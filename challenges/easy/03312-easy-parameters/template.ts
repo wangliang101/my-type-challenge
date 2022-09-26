91f397db7fcc57156fc88c290940858166ed6591
wliang <wliang@infervision.com>
2022-09-26 20:03:41 +0800

    03057 03060 03312

--- challenges/easy/03312-easy-parameters/template.ts
+++ challenges/easy/03312-easy-parameters/template.ts
@@ -1,1 +1,3 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer X) => any ? X : never;
+
+// type a = MyParameters<typeof bar>;
